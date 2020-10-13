7ef0f1f59ed4bfd96c0d3c280d74d329d29bb643
Racon94 <694543285@qq.com>
2020-10-13 17:42:18 +0800

    834

--- solution/834.树中距离之和.cpp
+++ solution/834.树中距离之和.cpp
@@ -1,6 +1,8 @@
 // @before-stub-for-debug-begin
 #include <vector>
 #include <string>
+#include <queue>
+#include <stack>
 #include "commoncppproblem834.h"
 
 using namespace std;
@@ -55,26 +57,155 @@ public:
     vector<int> sumOfDistancesInTree(int N, vector<vector<int>> &edges)
     {
         vector<vector<int>> list(N);
-        // 制作子节点表，由小到大排
+
         for (int i = 0; i < edges.size(); i++)
         {
-            // edges[i][0] edges[i][1]小的做list的行，大的做列
-            if (edges[i][0] < edges[i][1])
+            // 横竖都加一遍
+            list[edges[i][0]].emplace_back(edges[i][1]);
+            list[edges[i][1]].emplace_back(edges[i][0]);
+        }
+
+        // 制作节点数表
+        stack<int> stk1;
+        queue<int> q1;
+        vector<int> parents(N, 0);
+        int a1;
+        int len1;
+
+        stk1.emplace(0);
+        q1.emplace(0);
+        while (!q1.empty())
+        {
+            // a1 = stk1.top();
+            a1 = q1.front();
+            q1.pop();
+
+            len1 = list[a1].size();
+            for (int i = 0; i < len1; i++)
             {
-                list[edges[i][0]].emplace_back(edges[i][1]);
-            }
-            else
-            {
-                list[edges[i][1]].emplace_back(edges[i][0]);
+                if (list[a1][i] == parents[a1])
+                {
+                    continue;
+                }
+                q1.emplace(list[a1][i]);
+                parents[list[a1][i]] = a1;
+                stk1.emplace(list[a1][i]);
             }
         }
-        vector<int> ans(N);
-        //计算ans0
+
+        vector<int> count(N, 1);
+        while (!stk1.empty())
+        {
+            a1 = stk1.top();
+            stk1.pop();
+            len1 = list[a1].size();
+            for (int i = 0; i < len1; i++)
+            {
+                if (list[a1][i] == parents[a1])
+                {
+                    continue;
+                }
+                count[a1] += count[list[a1][i]];
+            }
+        }
+
+        // 计算ans0
+        vector<int> ans(N, 0);
+        int layer;
+        while (!q1.empty())
+        {
+            // a1 = stk1.top();
+            a1 = q1.front();
+            q1.pop();
+
+            len1 = list[a1].size();
+            for (int i = 0; i < len1; i++)
+            {
+                if (list[a1][i] == parents[a1])
+                {
+                    continue;
+                }
+                q1.emplace(list[a1][i]);
+                ans[0]+=count[list[a1][i]];
+            }
+        }
 
 
+        // while (!stk1.empty())
+        // {
+        //     a1 = stk1.top();
+
+        // }
+
+        // // 逆序制作节点数表
+        // vector<int> count(N, 0);
+        // for (int i = N - 1; i >= 0; i--)
+        // {
+        //     int len = list[i].size();
+        //     for (int j = 0; j < len; j++)
+        //     {
+        //         count[i] += count[list[i][j]];
+        //     }
+        //     count[i]++;
+        // }
+
+        vector<int> ans(N, 0);
+        // //计算ans0
+        // queue<int> qi;
+        // queue<int> ql;
+        // qi.emplace(0);
+        // ql.emplace(0);
+        // while (!qi.empty())
+        // {
+        //     int cur = qi.front();
+        //     qi.pop();
+        //     int layer = ql.front();
+        //     ql.pop();
+        //     ans[0] += layer;
+        //     int len = list[cur].size();
+        //     for (int i = 0; i < len; i++)
+        //     {
+        //         qi.emplace(list[cur][i]);
+        //         ql.emplace(layer + 1);
+        //     }
+        // }
+        // qi.emplace(0);
+        // ql.emplace(0);
+        // // qi存当前节点，ql存父节点
+        // // 计算其他ans
+        // while (!qi.empty())
+        // {
+        //     int cur = qi.front();
+        //     int prev = ql.front();
+        //     ql.pop();
+        //     qi.pop();
+        //     int len = list[cur].size();
+        //     for (int i = 0; i < len; i++)
+        //     {
+        //         qi.emplace(list[cur][i]);
+        //         ql.emplace(cur);
+        //     }
+        //     if (cur == 0)
+        //     {
+        //         continue;
+        //     }
+        //     else
+        //     {
+        //         ans[cur] = ans[prev] + N - count[cur] - count[cur];
+        //     }
+        // }
+
+        return ans;
     }
 
-    
+    //     // 求ans0
+    //     void dfs1(vector<int> &ans, int cur)
+    //     {
+    //     }
 
+    //     // 求其他ans
+    //     void dfs2(vector<int> &ans, int cur)
+    //     {
+    //     }
 };
 // @lc code=end
