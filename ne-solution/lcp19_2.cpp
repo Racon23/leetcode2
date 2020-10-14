2bbeef7f0867e9ebedc0f5778ef249ac0b1455d4
Racon94 <694543285@qq.com>
2020-10-14 23:28:46 +0800

    lcp19_2

--- ne-solution/lcp19_2.cpp
+++ ne-solution/lcp19_2.cpp
@@ -11,23 +11,21 @@ public:
     int minimumOperations(string leaves)
     {
         int n = leaves.size();
-        vector<vector<int>> f(n, vector<int>(3));
-        f[0][0] = leaves[0]=='y';
-        f[0][1] = n + 1;
-        f[0][2] = n + 1;
-        f[1][2] = n + 1;
+        vector<int> f(n);
+        f[0] = leaves[0] == 'y';
+        int c0 = f[0], c1 = n + 1, c2 = n + 1;
         for (int i = 1; i < n; i++)
         {
             int isRed = leaves[i] == 'r';
             int isYellow = leaves[i] == 'y';
-            f[i][0] = f[i - 1][0] + isYellow;
-            f[i][1] = min(f[i - 1][0], f[i - 1][1]) + isRed;
             if (i >= 2)
             {
-                f[i][2] = min(f[i - 1][1], f[i - 1][2]) + isYellow;
+                f[i] = c2 = min(c1, c2) + isYellow;
             }
+            c1 = min(c0, c1) + isRed;
+            c0 = c0 + isYellow;
         }
-        return f[n - 1][2];
+        return f[n - 1];
     }
 };
 
