af8b37002e7823db787aa4de3ab6cfb48883eaff
Racon94 <694543285@qq.com>
2020-10-28 18:01:03 +0800

    q845

--- solution/1024.视频拼接_2.cpp
+++ solution/1024.视频拼接_2.cpp
@@ -1,6 +1,7 @@
 // @before-stub-for-debug-begin
 #include <vector>
 #include <string>
+#include <algorithm>
 #include "commoncppproblem1024.h"
 
 using namespace std;
@@ -19,18 +20,31 @@ class Solution
 public:
     int videoStitching(vector<vector<int>> &clips, int T)
     {
-        vector<int> dp(T + 1, INT_MAX);
-        dp[0] = 0;
+        vector<int> maxn(T);
         int n = clips.size();
-        for (int i = 1; i <= T; i++)
+        int last = 0, prev = 0, res = 0;
+        for (vector<int> &it : clips)
         {
-            for(auto &it:clips){
-                if(it[0]<i&&it[1]>=i){
-                    dp[i]=min(dp[i],dp[it[0]]+1);
-                }
+            if (it[0] < T)
+            {
+                maxn[it[0]] = max(maxn[it[0]], it[1]);
             }
         }
-        return dp[T]==INT_MAX?-1:dp[T];
+        for (int i = 0; i < T; i++)
+        {
+            last = max(last, maxn[i]);
+            if (i == last)
+            {
+                return -1;
+            }
+            if (i == prev)
+            {
+                res++;
+                prev = last;
+            }
+        }
+        return res;
     }
 };
+
 // @lc code=end
