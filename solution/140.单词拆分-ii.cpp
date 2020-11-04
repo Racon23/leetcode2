b74a4101a40ea9a01f8ea09a5eff86ba5d07c586
Racon94 <694543285@qq.com>
2020-11-04 12:00:35 +0800

    q140

--- solution/140.单词拆分-ii.cpp
+++ solution/140.单词拆分-ii.cpp
@@ -15,6 +15,7 @@ using namespace std;
  */
 
 // @lc code=start
+// 加上记忆
 class Solution
 {
 public:
@@ -29,16 +30,19 @@ public:
             maxn = maxn > it.size() ? maxn : it.size();
         }
         vector<string> res;
-        dfs(s, 0, wdset, "", res, maxn, n);
-        return res;
+        vector<vector<string>> rem(n + 1); // 记录i之后的单词的组合
+        vector<int> flags(n + 1, 2);       // 0 false 1 true 2 non-check
+        dfs(s, 0, wdset, "", res, maxn, n, rem, flags);
+        return rem[0];
     }
 
-    void dfs(string &s, int st, unordered_set<string> &wdset, string str, vector<string> &res, int maxn, int n)
+    void dfs(string &s, int st, unordered_set<string> &wdset, string str, vector<string> &res, int maxn, int n, vector<vector<string>> &rem, vector<int> &flags)
     {
         if (st == s.size())
         {
             // 去掉空格
-            res.emplace_back(str.substr(0, str.size() - 1));
+            // res.emplace_back(str.substr(0, str.size() - 1));
+            flags[st] = 1;
             return;
         }
 
@@ -47,12 +51,31 @@ public:
             string tmp = s.substr(st, i);
             if (wdset.find(tmp) != wdset.end())
             {
-                string rem = str;
-                str += tmp + " ";
-                dfs(s, st + i, wdset, str, res, maxn, n);
-                str = rem;
+                // string rem = str;
+                // str += tmp + " ";
+                if (flags[st + i] == 2)
+                {
+                    dfs(s, st + i, wdset, str, res, maxn, n, rem, flags);
+                }
+                if (flags[st + i] == 1)
+                {
+                    if (st + i == s.size())
+                    {
+                        rem[st].emplace_back(tmp);
+                    }
+                    else
+                    {
+                        // 已经遍历过，是匹配的
+                        for (auto &it : rem[st + i])
+                        {
+                            // rem[st].emplace_back(tmp + " " + it);
+                            rem[st].emplace_back(tmp + " " + it);
+                        }
+                    }
+                }
             }
         }
+        // flags[st] = flags[st] == 2 ? 0 : flags[st];
         return;
     }
 };
