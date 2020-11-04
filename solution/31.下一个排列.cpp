23eb0678a2bdc34f0a590b00317c8acf4d9f1f75
Racon94 <694543285@qq.com>
2020-11-04 22:22:44 +0800

    q31

---
+++ solution/31.下一个排列.cpp
@@ -0,0 +1,69 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include <algorithm>
+#include "commoncppproblem31.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=31 lang=cpp
+ *
+ * [31] 下一个排列
+ *
+ * https://leetcode-cn.com/problems/next-permutation/description/
+ *
+ * algorithms
+ * Medium (34.62%)
+ * Likes:    720
+ * Dislikes: 0
+ * Total Accepted:    96.9K
+ * Total Submissions: 280K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+ * 
+ * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+ * 
+ * 必须原地修改，只允许使用额外常数空间。
+ * 
+ * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+ * 1,2,3 → 1,3,2
+ * 3,2,1 → 1,2,3
+ * 1,1,5 → 1,5,1
+ * 
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int i = n - 1;
+        while (i >= 1)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                int j = i;
+                while (j < n && nums[j] > nums[i - 1])
+                {
+                    j++;
+                }
+                int temp = nums[i - 1];
+                nums[i - 1] = nums[j - 1];
+                nums[j - 1] = temp;
+                reverse(nums.begin() + i, nums.end());
+                break;
+            }
+            i--;
+        }
+        if (i == 0)
+        {
+            reverse(nums.begin(), nums.end());
+        }
+    }
+};
+// @lc code=end
---
+++ solution/32.最长有效括号.cpp
@@ -0,0 +1,50 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem32.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=32 lang=cpp
+ *
+ * [32] 最长有效括号
+ *
+ * https://leetcode-cn.com/problems/longest-valid-parentheses/description/
+ *
+ * algorithms
+ * Hard (34.03%)
+ * Likes:    1053
+ * Dislikes: 0
+ * Total Accepted:    110.1K
+ * Total Submissions: 323.5K
+ * Testcase Example:  '"(()"'
+ *
+ * 给定一个只包含 '(' 和 ')' 的字符串，找出最长的包含有效括号的子串的长度。
+ * 
+ * 示例 1:
+ * 
+ * 输入: "(()"
+ * 输出: 2
+ * 解释: 最长有效括号子串为 "()"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: ")()())"
+ * 输出: 4
+ * 解释: 最长有效括号子串为 "()()"
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+
+    }
+};
+// @lc code=end
+
