6e84daf5b1ac3aad9d943490e7bfec8a680ddb82
Racon94 <694543285@qq.com>
2020-11-09 17:09:28 +0800

    q127 bfs

--- solution/127.单词接龙.cpp
+++ solution/127.单词接龙.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <unordered_set>
+#include <queue>
 #include "commoncppproblem127.h"
 
 using namespace std;
@@ -19,18 +20,57 @@ class Solution
 public:
     int ladderLength(string beginWord, string endWord, vector<string> &wordList)
     {
-        int i = 0;
         int count = 0;
         int n = wordList.size();
-        unordered_set<string> wdset;
-        for(auto &it:wordList){
-            wdset.emplace(it);
-        }
-
-        while (beginword != endword || count < n)
+        unordered_set<string> visited;
+        queue<string> q;
+        int len;
+        q.emplace(beginWord);
+        visited.emplace(beginWord);
+        len = q.size();
+        while (len > 0)
         {
-            
+            count++;
+            for (int i = 0; i < len; i++)
+            {
+                string s = q.front();
+                q.pop();
+                for (auto &it : wordList)
+                {
+                    if (visited.find(it) != visited.end())
+                    {
+                        continue;
+                    }
+                    if (!canConvert(s, it))
+                    {
+                        continue;
+                    }
+                    if (endWord == it)
+                    {
+                        return count + 1;
+                    }
+                    q.emplace(it);
+                    visited.emplace(it);
+                }
+            }
+            len = q.size();
         }
+        return 0;
+    }
+
+    bool canConvert(string s1, string s2)
+    {
+        if (s1.length() != s2.length())
+            return false;
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++)
+        {
+            if (s1[i] != s2[i])
+            {
+                count++;
+            }
+        }
+        return count == 1;
     }
 };
 // @lc code=end
