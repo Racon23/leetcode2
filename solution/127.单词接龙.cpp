3c08438fc52abfb7593be2d0db274666f587a531
Racon94 <694543285@qq.com>
2020-11-09 17:53:20 +0800

    双向bfs

--- solution/127.单词接龙.cpp
+++ solution/127.单词接龙.cpp
@@ -15,31 +15,58 @@ using namespace std;
  */
 
 // @lc code=start
+// double bfs
 class Solution
 {
 public:
     int ladderLength(string beginWord, string endWord, vector<string> &wordList)
     {
-        int count = 0;
         int n = wordList.size();
-        // unordered_set<string> visited;
-        vector<bool> visited(n, false);
-        queue<string> q;
-        int len;
-        q.emplace(beginWord);
-        // visited.emplace(beginWord);
-        len = q.size();
-        while (len > 0)
+        unordered_set<int> visited1;
+        unordered_set<int> visited2;
+        queue<int> q1; // 正向bfs
+        queue<int> q2; // 反向bfs
+        int len1;
+        int len2;
+        for (int i = 0; i < n; i++)
+        {
+            if (wordList[i] == endWord)
+            {
+                q2.emplace(i);
+                visited2.emplace(i);
+            }
+        }
+        if (q2.empty())
+        {
+            return 0;
+        }
+        wordList.emplace_back(beginWord);
+        q1.emplace(n);
+        visited1.emplace(n);
+        len1 = q1.size();
+        len2 = q2.size();
+        int count = 0;
+        while (len1 > 0 && len2 > 0)
         {
             count++;
-            for (int i = 0; i < len; i++)
+            if (len1 > len2)
             {
-                string s = q.front();
-                q.pop();
+                queue<int> temp = q2;
+                q2 = q1;
+                q1 = temp;
+                len1 = len2;
+                len2 = q2.size();
+                unordered_set<int> tv = visited2;
+                visited2 = visited1;
+                visited1 = tv;
+            }
+            for (int i = 0; i < len1; i++)
+            {
+                string s = wordList[q1.front()];
+                q1.pop();
                 for (int j = 0; j < n; j++)
                 {
-                    // if (visited.find(wordList[j]) != visited.end())
-                    if (visited[j])
+                    if (visited1.find(j) != visited1.end())
                     {
                         continue;
                     }
@@ -47,24 +74,24 @@ public:
                     {
                         continue;
                     }
-                    if (endWord == wordList[j])
+                    if (visited2.find(j) != visited2.end())
                     {
                         return count + 1;
                     }
-                    q.emplace(wordList[j]);
-                    // visited.emplace(it);
-                    visited[j] = true;
+                    q1.emplace(j);
+                    visited1.emplace(j);
                 }
             }
-            len = q.size();
+            len1 = q1.size();
+            len2 = q2.size();
         }
         return 0;
     }
 
     bool canConvert(string s1, string s2)
     {
-        if (s1.length() != s2.length())
-            return false;
+        // if (s1.length() != s2.length())
+        //     return false;
         int count = 0;
         for (int i = 0; i < s1.length(); i++)
         {
