5865a13f08fe6afe2d8fdffcecb26db15c29224b
Racon94 <694543285@qq.com>
2020-11-17 11:05:36 +0800

    q32

--- solution/32.最长有效括号.cpp
+++ solution/32.最长有效括号.cpp
@@ -40,11 +40,31 @@ using namespace std;
  */
 
 // @lc code=start
-class Solution {
+// 动态规划
+class Solution
+{
 public:
-    int longestValidParentheses(string s) {
-
+    int longestValidParentheses(string s)
+    {
+        int n = s.size();
+        vector<int> dp(n, 0);
+        int maxans = 0;
+        for (int i = 1; i < n; i++)
+        {
+            if (s[i] == ')')
+            {
+                if (s[i - 1] == '(')
+                {
+                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
+                }
+                else if (i - dp[i - 1] - 1 >= 0 && s[i - dp[i - 1] - 1] == '(')
+                {
+                    dp[i] = dp[i - 1] + 2 + ((i - dp[i - 1] - 2) >= 0 ? dp[i - dp[i - 1] - 2] : 0);
+                }
+                maxans = max(maxans, dp[i]);
+            }
+        }
+        return maxans;
     }
 };
 // @lc code=end
-
