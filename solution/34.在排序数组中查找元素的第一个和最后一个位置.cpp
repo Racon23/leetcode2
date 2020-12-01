edca1d5ec468061a69853e26b2436fbb87032d70
Racon94 <694543285@qq.com>
2020-12-01 11:23:09 +0800

    q34

--- solution/34.在排序数组中查找元素的第一个和最后一个位置.cpp
+++ solution/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -19,43 +19,47 @@ public:
     vector<int> searchRange(vector<int> &nums, int target)
     {
         int n = nums.size();
+
+        int left, right, mid;
+        left = 0;
+        right = n - 1;
         vector<int> ret(2, -1);
-        int mid = 0;
-        int l = 0, r = n - 1;
-        while (l <= r)
+        while (left <= right)
         {
-            mid = (l + r) / 2;
-            if (nums[mid] >= target)
+            mid = (left + right) / 2;
+            if (nums[mid] == target && (mid == 0 || nums[mid - 1] < target))
             {
-                if (nums[mid] == target && (mid == 0 || nums[mid - 1] < target))
-                {
-                    ret[0] = mid;
-                    break;
-                }
-                r = mid - 1;
+                ret[0] = mid;
+                break;
+            }
+            else if (nums[mid] >= target)
+            {
+                right = mid - 1;
             }
             else
             {
-                l = mid + 1;
+                left = mid + 1;
             }
         }
 
-        l = 0, r = n - 1;
-        while (l <= r)
+
+        left = 0;
+        right = n - 1;
+        while (left <= right)
         {
-            mid = (l + r) / 2;
-            if (nums[mid] <= target)
+            mid = (left + right) / 2;
+            if (nums[mid] == target && (mid == n-1 || nums[mid + 1] > target))
             {
-                if (nums[mid] == target && (mid == n - 1 || nums[mid + 1] > target))
-                {
-                    ret[1] = mid;
-                    break;
-                }
-                l = mid + 1;
+                ret[1] = mid;
+                break;
+            }
+            else if (nums[mid] <= target)
+            {
+                left = mid + 1;
             }
             else
             {
-                r = mid - 1;
+                right = mid - 1;
             }
         }
 
