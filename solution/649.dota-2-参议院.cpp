0263222afbd64b084abb4b7654fbe56da936767c
Racon94 <694543285@qq.com>
2020-12-11 09:18:15 +0800

    q649

---
+++ solution/649.dota-2-参议院.cpp
@@ -0,0 +1,23 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem649.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=649 lang=cpp
+ *
+ * [649] Dota2 参议院
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+
+    }
+};
+// @lc code=end
+
