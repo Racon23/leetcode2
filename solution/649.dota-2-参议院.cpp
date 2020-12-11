394582e2b6064c9115cdfb235ca7a1fe9369cea0
Racon94 <694543285@qq.com>
2020-12-11 15:23:46 +0800

    q649

--- solution/649.dota-2-参议院.cpp
+++ solution/649.dota-2-参议院.cpp
@@ -13,11 +13,57 @@ using namespace std;
  */
 
 // @lc code=start
-class Solution {
+// 递归
+class Solution
+{
 public:
-    string predictPartyVictory(string senate) {
+    string predictPartyVictory(string senate)
+    {
+        int len = senate.size();
+        vector<char> vc(len);
+        vc.assign(senate.begin(), senate.end());
+        return nextSenate(vc, 0, 0);
+    }
 
+    string nextSenate(vector<char> &vc, int rb, int db)
+    {
+        vector<char> next;
+        int rc = 0, dc = 0;
+        for (auto &x : vc)
+        {
+            if (x == 'R')
+            {
+                if (rb == 0)
+                {
+                    db++;
+                    rc++;
+                    next.emplace_back('R');
+                }
+                else
+                {
+                    rb--;
+                }
+            }
+            else
+            {
+                if (db == 0)
+                {
+                    rb++;
+                    dc++;
+                    next.emplace_back('D');
+                }
+                else
+                {
+                    db--;
+                }
+            }
+        }
+        if (dc == 0)
+            return "Radiant";
+        else if (rc == 0)
+            return "Dire";
+        else
+            return nextSenate(next,rb,db);
     }
 };
 // @lc code=end
-
