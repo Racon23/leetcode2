7374cf256b699cfa687a8dda1e9adad4d6fe7f20
Racon94 <694543285@qq.com>
2021-01-06 16:54:17 +0800

    q399

--- solution/399.除法求值.cpp
+++ solution/399.除法求值.cpp
@@ -28,39 +28,63 @@ public:
         {
             if (variables.find(equations[i][0]) == variables.end())
             {
-                variables[equations[i][0]]=nvars++;
+                variables[equations[i][0]] = nvars++;
             }
             if (variables.find(equations[i][1]) == variables.end())
             {
-                variables[equations[i][1]]=nvars++;
+                variables[equations[i][1]] = nvars++;
             }
         }
 
         // 记录点之间的距离 权值
-        vector<vector<pair<int,double>>> edges(nvars);
-        for(int i=0;i<n;i++){
-            int va = variables[equations[i][0]],vb=variables[equations[i][1]];
+        vector<vector<pair<int, double>>> edges(nvars);
+        for (int i = 0; i < n; i++)
+        {
+            int va = variables[equations[i][0]], vb = variables[equations[i][1]];
             edges[va].push_back(make_pair(vb, values[i]));
-            edges[vb].push_back(make_pair(va, 1.0/values[i]));
+            edges[vb].push_back(make_pair(va, 1.0 / values[i]));
         }
 
         // 开始广度优先
         vector<double> ret;
-        for(const auto &q:queries){
+        for (const auto &q : queries)
+        {
             double result = -1.0;
-            int ia = q[0],ib=q[1];
-            if(variables.find(ia)!=variables.end()&&variables.find(ib)!=variables.end()){
-                if(ia==ib){
+
+            if (variables.find(q[0]) != variables.end() && variables.find(q[1]) != variables.end())
+            {
+                int ia = variables[q[0]], ib = variables[q[1]];
+                if (ia == ib)
+                {
                     result = 1.0;
-                }else{
+                }
+                else
+                {
                     // 记录搜索时的点
                     queue<int> points;
                     points.emplace(ia);
-                    vector<double> 
-                }
+                    vector<double> ratios(nvars, -1.0);
+                    ratios[ia] = 1.0;
+                    while(!points.empty() && ratios[ib] < 0)
+                    {
+                        int x = points.front();
+                        points.pop();
 
+                        for (const auto [y, val] : edges[x])
+                        {
+                            if (ratios[y] < 0)
+                            {
+                                ratios[y] = ratios[x] * val;
+                                points.emplace(y);
+                            }
+                        }
+                    }
+                    result = ratios[ib];
+                }
             }
+            ret.emplace_back(result);
         }
+        return ret;
     }
 };
 // @lc code=end
