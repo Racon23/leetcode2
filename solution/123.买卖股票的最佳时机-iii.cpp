16f62ac7d8fbf4821506a096b18c809f5c6638b5
Racon94 <694543285@qq.com>
2021-01-13 11:40:38 +0800

    q123

--- solution/123.买卖股票的最佳时机-iii.cpp
+++ solution/123.买卖股票的最佳时机-iii.cpp
@@ -16,7 +16,19 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
+        int n = prices.size();
+        int stock1 = -prices[0];
+        int stock2 = -prices[0];
+        int money1 = 0;
+        int money2 = 0;
+
+        for(int i=1;i<n;i++){
+            stock1 = max(stock1,-prices[i]);
+            money1 = max(money1, stock1+prices[i]);
+            stock2 = max(stock2, money1-prices[i]);
+            money2 = max(money2, stock2+prices[i]);
+        }
+        return money2;
     }
 };
 // @lc code=end
