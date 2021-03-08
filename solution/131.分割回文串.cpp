3c599ba8324d0880a7178c637a99ee1ec66b0dbc
Racon94 <694543285@qq.com>
2021-03-08 18:01:30 +0800

    q131

---
+++ solution/131.分割回文串.cpp
@@ -0,0 +1,38 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include "commoncppproblem131.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=131 lang=cpp
+ *
+ * [131] 分割回文串
+ */
+
+// @lc code=start
+class Solution {
+    vector<vector<int>> rem;
+    vector<vector<string>> ret;
+
+public:
+    bool isPalindrome(string s){
+        return true;
+    }
+
+    void dfs(string s,int i,int j){
+
+    }
+    
+    vector<vector<string>> partition(string s) {
+        int len = s.length();
+        re
+        dfs()
+    }
+
+
+};
+// @lc code=end
+
