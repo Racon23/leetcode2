2c86ff6229d788812e79e4e11562ac3a20a5b091
Racon94 <694543285@qq.com>
2021-03-09 10:03:06 +0800

    q131

--- solution/131.分割回文串.cpp
+++ solution/131.分割回文串.cpp
@@ -13,26 +13,47 @@ using namespace std;
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
     vector<vector<int>> rem;
     vector<vector<string>> ret;
+    vector<string> tmp;
+    int len;
 
 public:
-    bool isPalindrome(string s){
-        return true;
+    int isPalindrome(string &s, int i, int j)
+    {
+        if (rem[i][j])
+            return rem[i][j];
+        if (i >= j)
+            return rem[i][j] = 1;
+        return rem[i][j] = (s[i] == s[j] ? isPalindrome(s, i + 1, j - 1) : -1);
     }
 
-    void dfs(string s,int i,int j){
-
+    void dfs(string &s, int i)
+    {
+        if (i == len)
+        {
+            ret.emplace_back(tmp);
+            return;
+        }
+        for (int k = i; k < len; k++)
+        {
+            if (isPalindrome(s, i, k) == 1)
+            {
+                tmp.push_back(s.substr(i, k - i + 1));
+                dfs(s, k + 1);
+                tmp.pop_back();
+            }
+        }
     }
-    
-    vector<vector<string>> partition(string s) {
-        int len = s.length();
-        re
-        dfs()
+
+    vector<vector<string>> partition(string s)
+    {
+        len = s.length();
+        rem.resize(len, vector<int>(len, 0));
+        dfs(s, 0);
+        return ret;
     }
-
-
 };
 // @lc code=end
-
