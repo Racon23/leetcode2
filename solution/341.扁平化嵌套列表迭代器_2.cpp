a15ebb0fbed7dc5235daaf7436c438cfa77b1c21
Racon94 <694543285@qq.com>
2021-03-23 10:33:16 +0800

    q341

---
+++ solution/341.扁平化嵌套列表迭代器_2.cpp
@@ -0,0 +1,73 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+#include <stack>
+#include "commoncppproblem341.h"
+
+using namespace std;
+// @before-stub-for-debug-end
+
+/*
+ * @lc app=leetcode.cn id=341 lang=cpp
+ *
+ * [341] 扁平化嵌套列表迭代器
+ */
+
+// @lc code=start
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator
+{
+    stack<pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator>> stk;
+
+public:
+    NestedIterator(vector<NestedInteger> &nestedList)
+    {
+        stk.emplace(nestedList.begin(),nestedList.end());
+    }
+
+    int next()
+    {
+        return stk.top().first++->getInteger();
+    }
+
+    bool hasNext()
+    {
+        while(!stk.empty()){
+            auto &p = stk.top();
+            if(p.first==p.second){
+                stk.pop();
+                continue;
+            }
+            if(p.first->isInteger()){
+                return true; 
+            }
+            auto &lst = p.first++->getList();
+            stk.emplace(lst.begin(),lst.end());
+        }
+        return false;
+    }
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
+// @lc code=end
